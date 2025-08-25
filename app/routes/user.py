a56f0497b8ff042f4917fb382403d90eb877d390
Bishwajit garai <102572447+Bishwajitgarai@users.noreply.github.com>
2025-08-25 23:56:17 +0530

    added file support for gemini

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,UploadFile,File,Query
 from fastapi.responses import JSONResponse
 from app.services.gemini_client import google_client,GeminiResponse
 from app.core.config import settings
@@ -25,6 +25,50 @@ async def get_query(query: str):
                 system_instruction="You are a chatbot"
             ),
             contents=[Part.from_text(text=query)],   # ✅ use actual query
+        )
+
+        if not response.candidates:
+            return JSONResponse(
+                status_code=500,
+                content={"status_code": 500, "message": "No response from Gemini"},
+            )
+
+        # Use instantiated objects.
+        response_data: GeminiResponse = response.parsed
+
+        
+        return JSONResponse(
+            status_code=500,
+            content={"status_code": 500, "message": response_data.model_dump()},
+        )
+
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "status_code": 500,
+                "message": "Error while fetching from Gemini",
+                "error": str(e),
+            },
+        )
+
+
+@user_router.post("/file")
+async def fileupload(file:UploadFile=File(...),query=Query(...)):
+    content_type=file.content_type
+    data=await file.read()
+    try:
+        response = google_client.models.generate_content(
+            model=settings.GEMINI_CHAT_MODEL,
+            config=GenerateContentConfig(
+                temperature=1.0,
+                top_k=40,      # must be int
+                top_p=0.95,
+                response_schema=GeminiResponse,
+                response_mime_type="application/json",
+                system_instruction="You are a chatbot follow user query"
+            ),
+            contents=[Part.from_bytes(data=data,mime_type=content_type),Part.from_text(text=query)],   # ✅ use actual query
         )
 
         if not response.candidates:
